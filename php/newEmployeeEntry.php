46656d47e25c3ebcd4f08ada99a98ca8390cd53a
chrifu4eve <chrifu4ever@gmail.com>
2024-01-19 15:21:39 +0100

    Modal Mitarbeiter zufügen funktioniert - Ohne Bild - Kommt nun

--- php/newEmployeeEntry.php
+++ php/newEmployeeEntry.php
@@ -1,32 +1,20 @@
 <?php
 require_once 'connectDB.php';
 
-function getDepartments($mysqli) {
-    $query = "SELECT Abteilungsname FROM Abteilungen";
-    $result = $mysqli->query($query);
-
-    $departments = [];
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $departments[] = $row['Abteilungsname'];
-        }
-    }
-    return $departments;
-}
-
 $db = new ConnectDB();
 $mysqli = $db->connect();
 
-//  Daten aus dem Formular lesen
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Daten aus dem Formular lesen
     $vorname = $_POST['vorname'] ?? null;
     $nachname = $_POST['nachname'] ?? null;
     $geburtsdatum = $_POST['geburtsdatum'] ?? null;
     $eintrittsdatum = $_POST['eintrittsdatum'] ?? null;
     $abteilung = $_POST['abteilung'] ?? null;
-    $foto = $_POST['bild'] ?? null;
+    $foto = $_POST['bild'] ?? null; // Hier sollten Sie $_FILES['bild'] verarbeiten
 
-    $query = "INSERT INTO Personal (Vorname, Nachname, Geburtsdatum, Eintrittsdatum, Autrittsdatum, Abteilung, Foto) VALUES (?, ?, ?, ?, ?,, NULL, ?)";
-    
+    // SQL-Statement vorbereiten
+    $query = "INSERT INTO Personal (Vorname, Nachname, Geburtsdatum, Eintrittsdatum, Austrittsdatum, Abteilung, Foto) VALUES (?, ?, ?, ?, NULL, ?, ?)";
     if ($stmt = $mysqli->prepare($query)) {
         $stmt->bind_param("ssssss", $vorname, $nachname, $geburtsdatum, $eintrittsdatum, $abteilung, $foto);
 
@@ -35,12 +23,12 @@ $mysqli = $db->connect();
         } else {
             echo "Fehler beim Anlegen des Mitarbeiters: " . $stmt->error;
         }
-
         $stmt->close();
     } else {
         echo "Fehler beim Vorbereiten des Statements: " . $mysqli->error;
     }
-
+}
 
 $mysqli->close();
+
 ?>
